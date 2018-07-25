34627f0b7d21004675043299514cbfa779718ebd
snazzafrass <snazzafrass@gmail.com>
2018-07-25 19:21:10 +0000

    automatically backed up by learn

---
+++ nested.rb
@@ -0,0 +1,7 @@
+closet = [shirts, pants, jackets, shoes]
+shirts = ["tank top", "t-shirt", "button up"]
+pants = ["jeans", "trousers"
+jackets = ["jean", "fur", "bomber"]
+shoes = ["tennis", "heels", "boots", "flats"]
+
+puts closet
