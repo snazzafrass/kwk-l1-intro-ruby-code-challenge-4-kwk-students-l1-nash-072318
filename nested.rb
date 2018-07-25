ad8db6e289f9addcea8b3aa0d24d30830a0d1f94
snazzafrass <snazzafrass@gmail.com>
2018-07-25 19:34:17 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -6,7 +6,11 @@ shoes = ["tennis", "heels", "boots", "flats"]
 
 closet = [shirts, pants, jackets, shoes]
 pants << "levis"
-print closet[3][0]
+print closet[2][1]
+print closet[1][0]
 
 
 # nested:"an array insde of an array"
+
+#try to print out only "fur", and then only "jeans"
+
