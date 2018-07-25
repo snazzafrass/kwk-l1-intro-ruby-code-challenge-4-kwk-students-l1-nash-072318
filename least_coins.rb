547deb5e9b2ab85f39af101e7172a0c2005b48e3
snazzafrass <snazzafrass@gmail.com>
2018-07-25 18:36:13 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,14 @@
 #write out your code here
 
-def least_coins(cents)
+def least_coins(coins)
+least_coins = {"quarters" => 1, "dimes" => 0, "nickels" => 0, "pennies" => 2}
+quarters=0 
+while coins > 25
+  coins =coins - 25
+  quarters+=1 
+end 
 
-#Code your answer here!
-
+puts quarters
 end
+
+least_coins(50)
