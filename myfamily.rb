fb57919896f731588f893e6a0d811912dbf137cb
snazzafrass <snazzafrass@gmail.com>
2018-07-25 20:10:55 +0000

    automatically backed up by learn

--- myfamily.rb
+++ myfamily.rb
@@ -1,6 +1,12 @@
 eubanks = {
   :tennesee_fam => {
     :nashville => ["melissa", "timothy","aimee"]
-    :
+  }
+  :south_carolina_fam => {
+    :greenville => ["tony", "wynee", "kennedy", "aj"],
+  :california_fam => {
+    
+  }
+  
   }
 }
