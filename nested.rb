ac1684d37499b220f6ec619571f504ba8256b58d
snazzafrass <snazzafrass@gmail.com>
2018-07-25 19:36:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -6,8 +6,8 @@ shoes = ["tennis", "heels", "boots", "flats"]
 
 closet = [shirts, pants, jackets, shoes]
 pants << "levis"
-print closet[2][1]
-print closet[1][0]
+puts closet[2][1]
+puts closet[1][0]
 
 
 # nested:"an array insde of an array"
