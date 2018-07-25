1fec555ed13e1f4dbbb8a5ffbf5d07436aeae633
snazzafrass <snazzafrass@gmail.com>
2018-07-25 19:24:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,8 +1,10 @@
 
 shirts = ["tank top", "t-shirt", "button up"]
-pants = ["jeans", "trousers"
+pants = ["jeans", "trousers"]
 jackets = ["jean", "fur", "bomber"]
 shoes = ["tennis", "heels", "boots", "flats"]
 
 closet = [shirts, pants, jackets, shoes]
 print closet
+
+# nested:"an array insde of an array"
